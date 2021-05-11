71be08631713a58dfc8b3b5cd67da9ee975c81cc
Pawel Cyprys <cyprysp@gmail.com>
2021-05-11 23:08:47 +0200

    Added rtl-sdr support

--- run_container.sh
+++ run_container.sh
@@ -2,6 +2,7 @@ docker run -ti --privileged --rm -e DISPLAY=$DISPLAY \
        -v /tmp/.X11-unix/:/tmp/.X11-unix \
        -v /dev/bus/usb:/dev/bus/usb \
        -v /dev/snd:/dev/snd \
+       --device /dev/snd \
        -v $HOME:/home/developer/working \
        --name gnuradio_hackrf \
        cyprysp/gnuradio_hackrf:latest
